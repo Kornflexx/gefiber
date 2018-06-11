83ff6839f10e5011e9438233fedb0c9453788dbd
Kornflexx <alex29820@gmail.com>
2018-06-11 23:10:16 +0200

    Change naming & improve performance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-export const BREAK_FIBER_MERGE = 'BREAK_FIBER_MERGE'
+export const BREAK_FIBER_CHAIN = 'BREAK_FIBER_CHAIN'
 
 export default function (...initialFibers) {
   const queues = [[], [], [], []]
@@ -10,45 +10,42 @@ export default function (...initialFibers) {
       for (let i = 0; i < 3; i += 1) {
         for (let j = 0; j < queues[i].length; j += 1) {
           result = yield* queues[i][j](...args, result)
-          if (result === BREAK_FIBER_MERGE) return
+          if (result === BREAK_FIBER_CHAIN) return
         }
       }
     } catch (exception) {
       for (let i = 0; i < queues[3].length; i += 1) {
         result = yield* queues[3][i](exception, ...args, result)
-        if (result === BREAK_FIBER_MERGE) return
+        if (result === BREAK_FIBER_CHAIN) return
       }
     }
   }
 
-  function makeAddFiber(type) {
-    return function addFiber(generator) {
-      queues[type].push(generator)
+  function makePush(type) {
+    const queue = queues[type]
+    return function push(generator) {
+      queue.push(generator)
       return worker
     }
   }
 
-  function mergeFibers(...fibers) {
-    fibers.forEach(function (fiber) {
-      queues[0].concat(fiber._queues[0])
-      queues[1].concat(fiber._queues[1])
-      queues[2].concat(fiber._queues[2])
-      queues[3].concat(fiber._queues[3])
-    })
-  }
-
   function merge(...fibers) {
-    mergeFibers(...fibers)
+    for (let i = 0; i < fibers.length; i += 1) {
+      queues[0].concat(fibers[i]._queues[0])
+      queues[1].concat(fibers[i]._queues[1])
+      queues[2].concat(fibers[i]._queues[2])
+      queues[3].concat(fibers[i]._queues[3])
+    }
     return worker
   }
 
   merge(...initialFibers)
 
   worker._queues = queues
-  worker.before = makeAddFiber(0)
-  worker.step = makeAddFiber(1)
-  worker.after = makeAddFiber(2)
-  worker.catch = makeAddFiber(3)
+  worker.before = makePush(0)
+  worker.step = makePush(1)
+  worker.after = makePush(2)
+  worker.catch = makePush(3)
   worker.merge = merge
   return worker
 }
